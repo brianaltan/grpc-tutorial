a0d1774256bd903b00f47ae11c8c51f771d8892c
Brian <brianaltan28@gmail.com>
2025-05-06 15:17:23 +0700

    Chat Service Implementation

--- src/grpc_client.rs
+++ src/grpc_client.rs
@@ -1,5 +1,14 @@
-use services::{payment_service_client::PaymentServiceClient, PaymentRequest,
-               transaction_service_client::TransactionServiceClient, TransactionRequest};
+use services::{
+    payment_service_client::PaymentServiceClient, PaymentRequest,    
+    transaction_service_client::TransactionServiceClient, TransactionRequest,
+    chat_service_client::ChatServiceClient, ChatMessage
+};
+
+use tonic::transport::Channel;
+use tokio::sync::mpsc;
+use tokio_stream::wrappers::ReceiverStream;
+use tokio::sync::mpsc::{Sender, Receiver};
+use tokio::io::{self, AsyncBufReadExt};
 
 pub mod services {
     tonic::include_proto!("services");
@@ -7,23 +16,55 @@ pub mod services {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = PaymentServiceClient::connect("http://[::1]:50051").await?;
-    let request = tonic::Request::new(PaymentRequest {
+    let mut payment_client = PaymentServiceClient::connect("http://[::1]:50051").await?;
+    let pay_req = tonic::Request::new(PaymentRequest {
         user_id: "user_123".to_string(),
         amount: 100.0,
     });
 
-    let response = client.process_payment(request).await?;
+    let response = payment_client.process_payment(pay_req).await?;
     println!("RESPONSE={:?}", response.into_inner());
 
     let mut transaction_client = TransactionServiceClient::connect("http://[::1]:50051").await?;
-    let request = tonic::Request::new(TransactionRequest {
+    let tx_req = tonic::Request::new(TransactionRequest {
         user_id: "user_123".to_string(),
     });
 
-    let mut stream = transaction_client.get_transaction_history(request).await?.into_inner();
-    while let Some(transaction) = stream.message().await? {
+    let mut tx_stream = transaction_client.get_transaction_history(tx_req).await?.into_inner();
+    while let Some(transaction) = tx_stream.message().await? {
         println!("Transaction: {:?}", transaction);
+    }
+
+    let channel = Channel::from_static("http://[::1]:50051").connect().await?;
+    let mut chat_client = ChatServiceClient::new(channel);
+    let (tx, rx): (Sender<ChatMessage>, Receiver<ChatMessage>) = mpsc::channel(32);
+
+    let tx_clone = tx.clone();
+    tokio::spawn(async move {
+        let stdin = io::stdin();
+        let mut reader = io::BufReader::new(stdin).lines();
+    
+        while let Ok(Some(line)) = reader.next_line().await {
+            if line.trim().is_empty() {
+                continue;
+            }
+            let message = ChatMessage {
+                user_id: "user_123".to_string(),
+                message: line,
+            };
+    
+            if tx_clone.send(message).await.is_err() {
+                eprintln!("Failed to send message to server.");
+                break;
+            }
+        }
+    });
+
+    let chat_req = tonic::Request::new(ReceiverStream::new(rx));
+    let mut chat_stream = chat_client.chat(chat_req).await?.into_inner();
+
+    while let Some(response) = chat_stream.message().await? {
+        println!("Server says: {:?}", response);
     }
 
     Ok(())
